aa1ea64537dffc32b3bdb4d26c567b89e8bbd346
Charles Knight <Charles-Knight@users.noreply.github.com>
2023-09-20 05:38:22 +0000

    Added hangman-cmd project for command line player

---
+++ hangman-cmd/src/main/java/com/github/charlesknight/HangmanCMD.java
@@ -0,0 +1,11 @@
+package com.github.charlesknight;
+
+/**
+ * HangmanCMD
+ *
+ */
+public class HangmanCMD {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Soon we will play hangman");
+    }
+}
